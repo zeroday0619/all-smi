cbaa8283ea8f73f05e7bfa5a90cc1f7e957afd20
Jeongkyu Shin <inureyes@gmail.com>
2024-08-10 21:15:02 +0900

    update: refactoring

--- src/main.rs
+++ src/main.rs
@@ -12,11 +12,10 @@ use crossterm::{
 };
 use chrono::Local;
 use std::io::{stdout, Write};
-use std::process::Command; // Command 타입 임포트
+use std::process::Command;
 
 fn ensure_sudo_permissions() {
     if cfg!(target_os = "macos") {
-        // Attempt to update sudo timestamp or prompt for password
         let status = Command::new("sudo")
             .arg("-v")
             .status()
@@ -29,6 +28,31 @@ fn ensure_sudo_permissions() {
     }
 }
 
+fn print_colored_text<W: Write>(
+    stdout: &mut W,
+    text: &str,
+    color: Color,
+    width: Option<usize>,
+) {
+    let adjusted_text = if let Some(w) = width {
+        if text.len() > w {
+            text.chars().take(w).collect::<String>()
+        } else {
+            format!("{:<width$}", text, width = w)
+        }
+    } else {
+        text.to_string()
+    };
+
+    execute!(
+        stdout,
+        SetForegroundColor(color),
+        Print(adjusted_text),
+        ResetColor
+    )
+    .unwrap();
+}
+
 fn draw_bar<W: Write>(
     stdout: &mut W,
     label: &str,
@@ -38,10 +62,9 @@ fn draw_bar<W: Write>(
     show_text: Option<String>,
 ) {
     let label_width = label.len();
-    let text_width = show_text.as_ref().map_or(0, |text| text.len()); // Calculate text length
-    let available_bar_width = width.saturating_sub(label_width + 4); // 4 for label and surrounding characters
+    let text_width = show_text.as_ref().map_or(0, |text| text.len());
+    let available_bar_width = width.saturating_sub(label_width + 4);
 
-    let _percentage = (value / max_value) * 100.0;
     let full_blocks = (value / max_value * available_bar_width as f64).floor() as usize;
     let remainder = (value / max_value * available_bar_width as f64) - full_blocks as f64;
     let filled_char = match remainder {
@@ -60,29 +83,40 @@ fn draw_bar<W: Write>(
     );
     let empty_bar = "▏".repeat(empty_width);
 
-    execute!(
-        stdout,
-        SetForegroundColor(Color::Blue),
-        Print(format!("{}: [", label)),
-        SetForegroundColor(Color::Green),
-        Print(filled_bar),
-        Print(empty_bar),
-        ResetColor
-    )
-    .unwrap();
+    print_colored_text(stdout, &format!("{}: [", label), Color::Blue, None);
+    print_colored_text(stdout, &filled_bar, Color::Green, None);
+    print_colored_text(stdout, &empty_bar, Color::Green, None);
 
     if let Some(text) = show_text {
-        execute!(
-            stdout,
-            SetForegroundColor(Color::White),
-            Print(format!("{:>width$}", text, width = text_width)),
-            ResetColor,
-            Print("] ")
-        )
-        .unwrap();
-    } else {
-        execute!(stdout, Print("] ")).unwrap();
+        print_colored_text(stdout, &text, Color::White, Some(text_width));
     }
+
+    execute!(stdout, Print("] ")).unwrap();
+}
+
+fn print_gpu_info<W: Write>(stdout: &mut W, index: usize, info: &GpuInfo, half_width: usize) {
+    let used_memory_gib = info.used_memory as f64 / (1024.0 * 1024.0 * 1024.0);
+    let total_memory_gib = info.total_memory as f64 / (1024.0 * 1024.0 * 1024.0);
+    let memory_text = format!("{:.2}/{:.2}Gi", used_memory_gib, total_memory_gib);
+    let gpu_percentage_text = format!("{:.2}%", info.utilization);
+    let freq_text = format!("{} MHz", info.frequency);
+    let power_text = format!("{:.2} W", info.power_consumption);
+
+    print_colored_text(stdout, &format!("DEVICE {}: ", index + 1), Color::Blue, None);
+    print_colored_text(stdout, &format!("{}  ", info.name), Color::White, None);
+    print_colored_text(stdout, "Total: ", Color::Blue, None);
+    print_colored_text(stdout, &format!("{:.2} GiB  ", total_memory_gib), Color::White, None);
+    print_colored_text(stdout, "Used: ", Color::Blue, None);
+    print_colored_text(stdout, &format!("{:.2} GiB  ", used_memory_gib), Color::White, None);
+    print_colored_text(stdout, "Temp.: ", Color::Blue, None);
+    print_colored_text(stdout, &format!("{}°C  ", info.temperature), Color::White, None);
+    print_colored_text(stdout, "FREQ: ", Color::Blue, None);
+    print_colored_text(stdout, &format!("{}  ", freq_text), Color::White, None);
+    print_colored_text(stdout, "POW: ", Color::Blue, None);
+    print_colored_text(stdout, &format!("{}\n", power_text), Color::White, None);
+
+    draw_bar(stdout, "GPU", info.utilization, 100.0, half_width, Some(gpu_percentage_text));
+    draw_bar(stdout, "MEM", used_memory_gib, total_memory_gib, half_width, Some(memory_text));
 }
 
 fn main() {
@@ -91,7 +125,6 @@ fn main() {
     let gpu_readers = get_gpu_readers();
     let mut stdout = stdout();
 
-    // Initialize the terminal screen and switch to alternate screen mode
     execute!(
         stdout,
         EnterAlternateScreen,
@@ -100,7 +133,6 @@ fn main() {
     .unwrap();
 
     loop {
-        // Check if the ESC key is pressed
         if event::poll(Duration::from_millis(100)).unwrap() {
             if let Event::Key(key_event) = event::read().unwrap() {
                 if key_event.code == KeyCode::Esc {
@@ -109,99 +141,29 @@ fn main() {
             }
         }
 
-        // Move the cursor to the top and avoid clearing the screen
         execute!(stdout, cursor::MoveTo(0, 0)).unwrap();
 
-        // Print the current time
         let current_time = Local::now().format("%Y-%m-%d %H:%M:%S").to_string();
-        execute!(stdout, Print(format!("{}\n", current_time))).unwrap();
+        print_colored_text(&mut stdout, &format!("{}\n", current_time), Color::White, None);
 
-        // Get the current terminal size
         let (cols, _) = size().unwrap();
-        let half_width = (cols / 2 - 2) as usize; // Adjusted width to prevent overflow
+        let half_width = (cols / 2 - 2) as usize;
 
-        let mut all_gpu_info: Vec<GpuInfo> = Vec::new();
+        let all_gpu_info: Vec<GpuInfo> = gpu_readers
+            .iter()
+            .flat_map(|reader| reader.get_gpu_info())
+            .collect();
 
-        for reader in &gpu_readers {
-            let gpu_info = reader.get_gpu_info();
-            all_gpu_info.extend(gpu_info);
-        }
-
-        // Print GPU information
         for (index, info) in all_gpu_info.iter().enumerate() {
-            let used_memory_gib = info.used_memory as f64 / (1024.0 * 1024.0 * 1024.0);
-            let total_memory_gib = info.total_memory as f64 / (1024.0 * 1024.0 * 1024.0);
-            let memory_text = format!("{:.2}/{:.2}Gi", used_memory_gib, total_memory_gib);
-            let gpu_percentage_text = format!("{:.2}%", info.utilization);
-            let freq_text = format!("{} MHz", info.frequency);
-            let power_text = format!("{:.2} W", info.power_consumption);
+            print_gpu_info(&mut stdout, index, info, half_width);
 
-            execute!(
-                stdout,
-                SetForegroundColor(Color::Blue),
-                Print(format!("DEVICE {}: ", index + 1)),
-                ResetColor,
-                SetForegroundColor(Color::White),
-                Print(format!("{}  ", info.name)),
-                SetForegroundColor(Color::Blue),
-                Print("Total: "),
-                ResetColor,
-                SetForegroundColor(Color::White),
-                Print(format!("{:.2} GiB  ", total_memory_gib)),
-                SetForegroundColor(Color::Blue),
-                Print("Used: "),
-                ResetColor,
-                SetForegroundColor(Color::White),
-                Print(format!("{:.2} GiB  ", used_memory_gib)),
-                SetForegroundColor(Color::Blue),
-                Print("Temp.: "),
-                ResetColor,
-                SetForegroundColor(Color::White),
-                Print(format!("{}°C  ", info.temperature)),
-                SetForegroundColor(Color::Blue),
-                Print("FREQ: "),
-                ResetColor,
-                SetForegroundColor(Color::White),
-                Print(format!("{}  ", freq_text)),
-                SetForegroundColor(Color::Blue),
-                Print("POW: "),
-                ResetColor,
-                SetForegroundColor(Color::White),
-                Print(format!("{}\n", power_text)),
-                ResetColor
-            )
-            .unwrap();
-
-            // Print GPU utilization bar (Adjusted for terminal width)
-            draw_bar(
-                &mut stdout,
-                "GPU",
-                info.utilization,
-                100.0,
-                half_width,
-                Some(gpu_percentage_text),
-            );
-
-            // Print MEM utilization bar (Adjusted for terminal width)
-            draw_bar(
-                &mut stdout,
-                "MEM",
-                used_memory_gib,
-                total_memory_gib,
-                half_width,
-                Some(memory_text),
-            );
-
-            // Add a blank line between devices
             if index < all_gpu_info.len() - 1 {
                 execute!(stdout, Print("\n\n")).unwrap();
             }
         }
 
-        // Wait for 1 second
         thread::sleep(Duration::from_secs(1));
     }
 
-    // Exit alternate screen mode and restore the original screen
     execute!(stdout, LeaveAlternateScreen).unwrap();
 }
