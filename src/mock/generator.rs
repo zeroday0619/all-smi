0fcd774fb0cede50e9995eb906b9aa885efdec04
Jeongkyu Shin <inureyes@gmail.com>
2025-08-07 21:03:27 +0900

    chore: add Apache 2.0 license

--- src/mock/generator.rs
+++ src/mock/generator.rs
@@ -1,5 +1,19 @@
 //! Metric generation utilities for creating realistic initial values
 
+// Copyright 2025 Lablup Inc. and Jeongkyu Shin
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 use crate::mock::constants::{DISK_SIZE_12TB, DISK_SIZE_1TB, DISK_SIZE_4TB, NUM_GPUS};
 use crate::mock::metrics::gpu::generate_uuid;
 use crate::mock::metrics::{CpuMetrics, GpuMetrics, MemoryMetrics, PlatformType};
--- src/mock/node.rs
+++ src/mock/node.rs
@@ -1,5 +1,19 @@
 //! Mock node representing a single server with multiple GPUs
 
+// Copyright 2025 Lablup Inc. and Jeongkyu Shin
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 use crate::mock::generator::{
     generate_cpu_metrics, generate_disk_metrics, generate_gpus, generate_memory_metrics,
 };
